e82c8f4552a9195f3ccbc4ae4582f4dc7c2d461b
JetsadaWijit <jetsadawijit@outlook.com>
2025-10-22 18:53:22 +0700

    feat: Add check license

--- server/papermc/engine/src/main/java/io/github/mcengine/papermc/currency/engine/MCEngineStoryPaperMC.java
+++ server/papermc/engine/src/main/java/io/github/mcengine/papermc/currency/engine/MCEngineStoryPaperMC.java
@@ -20,6 +20,15 @@ public class MCEngineStoryPaperMC extends JavaPlugin {
             return;
         }
 
+        String license = getConfig().getString("licenses.license", "free"); 
+        if (!license.equalsIgnoreCase("free")) { 
+            getLogger().warning("Plugin is disabled in config.yml.");
+            getLogger().warning("Invalid license.");
+            getLogger().warning("Check license or use \"free\".");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
         // Load extensions
         MCEngineCoreApi.loadExtensions(
             this,
